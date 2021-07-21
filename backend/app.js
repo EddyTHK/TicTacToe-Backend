5fa2d7594da865f41af12d1b9170aff42607d2cb
EddyTeo <teoeddy46.20@ichat.sp.edu.sg>
2021-07-21 11:19:34 +0800

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,55 @@
+// imports
+const express = require('express');
+const socket = require('socket.io');
+const {nanoid}= require('nanoid');
+
+const Game = require('../logic/player.js')
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000');
+});
+
+// Static files
+app.use(express.static('../website'));
+
+const sessInfo ={};
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('Socket connected: ', socket.id);
+
+    // Create Game (Player 1)
+    socket.on('createSess', function(name){
+        var sessionID = nanoid(10);
+        console.log(sessionID);
+        console.log('username: ' + name);
+
+        sessInfo[sessionID] = new Game(name,socket,sessionID);
+        
+        socket.emit("session-created", {
+            id: sessionID,
+            name: name
+        });
+
+        socket.on("disconnect", ()=> {
+            try{
+                socket.broadcast.emit("user-disconnected");
+            }
+            catch(err){
+                ;
+            }
+            
+            delete sessInfo[sessionID];
+            console.log("session deleted");
+        });
+    });
+
+    // TODO Join Game (Player 2)
+    // socket.on("join-session", function(data) {
+
+    // })
+});
