e7b9d25fe4215b93d1d6bab98fde048c650f3496
EddyTeo <teoeddy46.20@ichat.sp.edu.sg>
2021-07-21 18:14:19 +0800

    update

---
+++ logic/player.js
@@ -0,0 +1,32 @@
+module.exports = class {
+    player1Info;
+    player2Info;
+
+    game = {
+        player1Name: "",
+        player2Name : "",
+        player1Score: 0,
+        player2Score: 0,
+        ties : 0,
+        player1Turn: true,
+        board: [
+            0,0,0,
+            0,0,0,
+            0,0,0,
+        ]
+    }
+
+    constructor(name, info, sessID) {
+        this.player1Name = name;
+        this.player1Info = info;
+        this.sessID = sessID;
+    }
+
+    JoinGame=(name,info)=>{
+        this.gameState.player2Name = name;
+        this.player2Info = info;
+    }
+}
+
+
+
