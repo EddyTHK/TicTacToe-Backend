67dbf2b2dab98bb9910c671dbc679ca3c1a8b728
EddyTeo <teoeddy46.20@ichat.sp.edu.sg>
2021-08-02 21:21:25 +0800

    implemented battle royale tic tac toe

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const { nanoid } = require('nanoid');
 const httpServer = require('http').createServer(app);
 
-var Game = require('./logic/player.js').Session;
-
+var roomNo = 0;
+var roomID;
 // App setup
 app.use(cors());
 
@@ -27,76 +27,87 @@ app.use(function (req, res) {
     return res.sendStatus(200);
 });
 
-var lobbyInfo = [];
-var gameInfo = {};
-
 io.on('connection', (socket) => {
     console.log('Socket connected: ', socket.id);
 
     // Create Game (Player 1)
-    socket.on('createSess', function (name) {
-        var sessionID = nanoid(10);
-        console.log(sessionID);
-        lobbyInfo.push(sessionID);
-        console.log('player 1 name: ' + name);
+    socket.on('createSess', function (data) {
+        ++roomNo
+        roomID = 'room-' + roomNo
+        var player1Name = data.name;
 
-        gameInfo[lobbyInfo[0]] = new Game(name, socket, sessionID);
+        // create and send player 1 to a room
+        socket.join(roomID);
+        console.log('player 1: ' + player1Name + " joined " + roomID + "!");
 
-        socket.emit("session-created", {
-            id: sessionID,
-            name: name
+        io.emit("session-created", {
+            name: player1Name,
+            room: roomID
+        });
+
+        socket.on('updated', function (data) {
+            var tile = data.tile;
+            var type = data.type;
+
+            console.log(tile, type)
+            socket.to(roomID).emit('swap', {
+                tile: tile,
+                type: type
+            });
         });
 
         // check if player 1 disconnected
         socket.on("disconnect", () => {
-            try {
-                socket.broadcast.emit("user-disconnected");
-            }
-            catch (err) {
-            }
+            socket.broadcast.emit('user-disconnected');
+            socket.leave(roomID);
+            --roomNo
 
-            lobbyInfo.pop();
-            console.log("Player 2 disconnected");
+            console.log("Player 1 disconnected");
         });
-    });
-
-
-    socket.on('gameStart', function () {
-        socket.emit("game",{
-            joined: true
-        });
-        
-        console.log('test');
     });
 
     //Join Game (Player 2)
     socket.on("join-session", function (data) {
-        var id = data.id;
-        var name = data.name;
-        console.log("player 2 name: " + name);
-        console.log(lobbyInfo);
+        var player2Name = data.player2Name;
 
-        if (lobbyInfo.length) {
-            for (var i = 0; i < lobbyInfo.length; i++) {
-                if (lobbyInfo[i] == id) {
-                    gameInfo[lobbyInfo[0]].joinGame(name, id);
-                    socket.emit("joined");
-                }
-            }
-        }else {
-            socket.emit("errorInJoining");
+        // retrieves the number of client(s) in the room
+        var client = io.sockets.adapter.rooms.get(`${data.room}`);
+        const numClient = client ? client.size : 0;
+
+        // check if roomNo and number in room is 1 
+        if (numClient == 1) {
+            // let the user join the room
+            socket.join(data.room);
+            console.log('player 2: ' + player2Name + " joined " + data.room + "!");
+
+            //let the client know that player 2 successfully joined the room
+            io.to(data.room).emit("joined", {
+                name: player2Name,
+                room: data.room
+            });
+
+            socket.on('updated', function (data) {
+                var tile = data.tile;
+                var type = data.type;
+
+                console.log(tile, type)
+                socket.to(roomID).emit('swap', {
+                    tile: tile,
+                    type: type
+                });
+            });
+            
+        } else {
+            socket.on("errorInJoining", {
+                error: "Error in joining! This room is already full."
+            });
         }
-        
 
         // check if player 2 disconnected
         socket.on("disconnect", () => {
-            try {
-                socket.broadcast.emit("user-disconnected");
-            }
-            catch (err) {
-            }
+            socket.broadcast.emit('user-disconnected');
+            socket.leave(client);
 
-            lobbyInfo.pop();
             console.log("Player 2 disconnected");
         });
     });
