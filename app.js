988196b124bd82930814cc4267f5fbd0a21b9582
EddyTeo <teoeddy46.20@ichat.sp.edu.sg>
2021-08-05 18:41:22 +0800

    included cloud database feature

--- app.js
+++ app.js
@@ -2,14 +2,42 @@
 const express = require('express');
 var app = express();
 const cors = require('cors');
-const { nanoid } = require('nanoid');
 const httpServer = require('http').createServer(app);
+const {MongoClient} = require('mongodb');
 
 var roomNo = 0;
 var roomID;
 // App setup
 app.use(cors());
 
+// write the winners' name into mongodb
+async function main() {
+
+    // Connection to MongoDB 
+    const uri = "mongodb+srv://Eddy:44dO9YmiyKdmAKzz@ades-ca3.d3kex.mongodb.net/ADES-CA3?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+
+    try {
+        // only continue running when successfully connected to db
+        await client.connect();
+        await recordPlayerWin(client, {
+        });
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    };
+}
+
+main().catch(console.error);
+async function recordPlayerWin(client, playerName) {
+    const result = await client.db("tic-tac-toe").collection("winnerInfo").insertOne(playerName);
+
+    console.log("New listing: " + result.insertedId);
+}
+
+// Allow CORS in socket.io
 const io = require('socket.io')(httpServer, {
     cors: {
         origin: "*",
@@ -57,20 +85,25 @@ io.on('connection', (socket) => {
             });
         });
 
+        // listen to frontend for the 'draw' event
         socket.on('draw', function () {
             console.log("Draw!");
             socket.to(roomID).emit('endDraw');
         });
 
+        // listen to frontend for the 'win' event
         socket.on('win', function (data) {
             var winner = data.winner;
 
+            insert().main(winner);
+
+            console.log("Winner: ", winner);
             io.emit('endWin', {
                 winner: winner
-            })
+            });
         });
 
-        socket.on('exit' , function () {
+        socket.on('exit', function () {
             socket.leave(roomID);
             console.log(`user ${socket.id} has left the room`);
         });
@@ -123,13 +156,14 @@ io.on('connection', (socket) => {
 
             socket.on('win', function (data) {
                 var winner = data.winner;
-    
+
+                console.log("Winner: ", winner);
                 io.emit('endWin', {
                     winner: winner
-                })
+                });
             });
-            
-            socket.on('exit' , function () {
+
+            socket.on('exit', function () {
                 socket.leave(roomID);
                 console.log(`user ${socket.id} has left the room`);
             });
